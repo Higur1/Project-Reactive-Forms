2fc4daa22330aa04605a66465535dbc915600495
Higur1 <higor.hungria466@gmail.com>
2026-02-16 18:42:27 -0300

    Creating the Phone List Logic

--- src/app/components/contact-informations/components/contact-phone-list/contact-phone-list.ts
+++ src/app/components/contact-informations/components/contact-phone-list/contact-phone-list.ts
@@ -1,6 +1,8 @@
-import { Component, Input } from '@angular/core';
+import { Component, Input, OnChanges, SimpleChanges } from '@angular/core';
 import { required } from '@angular/forms/signals';
 import { PhoneList } from '../../../../types/phone-list';
+import { PhoneTypeEnum } from '../../../../enums/phone-type.enum';
+import { IPhone } from '../../../../interfaces/user/phone.interface';
 
 @Component({
   selector: 'app-contact-phone-list',
@@ -8,6 +10,36 @@ import { PhoneList } from '../../../../types/phone-list';
   templateUrl: './contact-phone-list.html',
   styleUrl: './contact-phone-list.scss',
 })
-export class ContactPhoneList {
-  @Input({ required: true}) userPhoneList: PhoneList | undefined = [];
+export class ContactPhoneList implements OnChanges {
+  phoneListToDisplay: any[] = [];
+  @Input({ required: true }) userPhoneList: PhoneList | undefined = [];
+
+  ngOnChanges(changes: SimpleChanges) {
+    const PHONE_LIST_LOADED = Array.isArray(changes['userPhoneList'].currentValue);
+
+    if (PHONE_LIST_LOADED) {
+      this.preparePhoneListToDisplay();
+    }
+  }
+  preparePhoneListToDisplay() {
+    this.phoneListToDisplay = [];
+
+    const phoneTypeDescriptionMap: { [key in PhoneTypeEnum]: string } = {
+      [PhoneTypeEnum.RESIDENTIAL]: 'Residential',
+      [PhoneTypeEnum.MOBILE]: 'Mobile',
+      [PhoneTypeEnum.EMERGENCY]: 'Emergency'
+    };
+
+    Object.keys(phoneTypeDescriptionMap).map(Number).forEach((phoneType: number) => {
+      const phoneFound = this.userPhoneList?.find((userPhone: IPhone) => userPhone.type === phoneType);
+
+      this.phoneListToDisplay.push({
+        type: phoneTypeDescriptionMap[phoneType as PhoneTypeEnum],
+        number: phoneFound ? this.formatPhoneNumber(phoneFound) : '-',
+      });
+    });
+  }
+  formatPhoneNumber(phone: IPhone) {
+    return `${phone.internationalCode} ${phone.areaCode} ${phone.number}`
+  }
 }
