c6abdc528544d94b2c1a00e09d6b0d3b19a7dbeb
Higur1 <higor.hungria466@gmail.com>
2026-03-04 15:35:25 -0300

    Creating a User Form

--- src/app/components/user-informations-container/user-informations-container.ts
+++ src/app/components/user-informations-container/user-informations-container.ts
@@ -1,5 +1,6 @@
 import { Component, Input, OnChanges, SimpleChanges } from '@angular/core';
 import { IUser } from '../../interfaces/user/user.interface';
+import { UserFormController } from './user-form-controller';
 
 @Component({
   selector: 'app-user-informations-container',
@@ -7,14 +8,20 @@ import { IUser } from '../../interfaces/user/user.interface';
   templateUrl: './user-informations-container.html',
   styleUrl: './user-informations-container.scss',
 })
-export class UserInformationsContainer implements OnChanges {
-  
+export class UserInformationsContainer extends UserFormController implements OnChanges {
+
   currentTabIndex: number = 0;
 
   @Input({ required: true }) userSelected: IUser = {} as IUser;
   @Input({ required: true }) isInEditMode: boolean = false;
 
-  ngOnChanges(_: SimpleChanges): void {
+  ngOnChanges(changes: SimpleChanges): void {
     this.currentTabIndex = 0;
+
+    const HAS_USER_SELECTED = changes['userSelected'] && Object.keys(changes['userSelected'].currentValue).length > 0;
+
+    if (HAS_USER_SELECTED) {
+      this.fulFillUserForm(this.userSelected);
+    }
   }
 }
