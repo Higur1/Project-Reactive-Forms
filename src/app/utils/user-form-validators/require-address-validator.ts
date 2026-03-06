4cfaae6a566bdc5d217e61689279c8e2d4312e81
Higur1 <higor.hungria466@gmail.com>
2026-03-06 19:52:17 -0300

    Refactor: Simplify requiredAddressValidator loop logic

--- src/app/utils/user-form-validators/require-address-validator.ts
+++ src/app/utils/user-form-validators/require-address-validator.ts
@@ -3,27 +3,27 @@ import { ValidationError } from "@angular/forms/signals";
 
 export const requiredAddressValidator: ValidatorFn = (control: AbstractControl): ValidationError | null => {
     const addressGroup = control as FormGroup;
-    const controlsToCheck = Object.keys(addressGroup.controls).filter((controlKey) => controlKey !== 'type' && controlKey !== 'typeDescription');
+
+    const controlsToCheck = Object.keys(addressGroup.controls)
+        .filter((controlKey) => controlKey !== 'type' && controlKey !== 'typeDescription');
+
     const hasAnyText = controlsToCheck.some((controlKey) => hasText(addressGroup.get(controlKey)));
 
-    for (const controlName of controlsToCheck) {
-        const control = addressGroup.get(controlName);
+    controlsToCheck.forEach(controlKey => {
+        const control = addressGroup.get(controlKey);
 
-        if (hasAnyText) {
-            if (!control?.value) {
-                control?.setErrors({ requiredAddressControl: true });
-                control?.markAsTouched();
-            } else {
-                control?.setErrors(null);
-            }
-        } else {
+        if (!hasAnyText || control?.value) {
             control?.setErrors(null);
+            return;
         }
-    }
+
+        control?.setErrors({ requiredAddressValidator: true });
+        control?.markAsTouched();
+    });
 
     return null;
 }
 
 const hasText = (control: AbstractControl | null): boolean => {
-    return !!control && control.value && control.value.toString().trim().length > 0;
+    return !!control?.value?.toString().trim();
 };
